c7f58a548b480e30781afed89fc15b1138c1f9ea
ocean shan <ocean.shan@gaiaworks.cn>
2023-06-15 16:48:06 +0800

    修改判断loadable逻辑，修改getLoading传参结构

--- lib/loading-extend.ts
+++ lib/loading-extend.ts
@@ -3,7 +3,7 @@ import { IAjax, IAjaxArgsOptions, IRequestResult, IOptions, ILoading, IRequestOp
 function loadingExtend({
     getLoading: _getLoading,
 }: {
-    getLoading: (loadingName: string | symbol) => ILoading;
+    getLoading: (props: { loadingName: string | symbol }) => ILoading;
 }): () => void {
     return function load(): void {
         const { beforeSend, responseEnd } = this as IAjax;
@@ -11,7 +11,7 @@ function loadingExtend({
         function getLoading(options: IOptions): ILoading | void {
             if (options.loadingName) {
                 if (_getLoading) {
-                    return _getLoading(options.loadingName);
+                    return _getLoading({ loadingName: options.loadingName });
                 }
                 if ((window as object)[options.loadingName]) {
                     const loading = (window as object)[options.loadingName] as ILoading;
@@ -26,20 +26,21 @@ function loadingExtend({
         }
 
         // 启用加载效果
-        (this as IAjax).beforeSend = (props: IAjaxArgsOptions): void => {
+        (this as IAjax).beforeSend = (props: IAjaxArgsOptions): IRequestResult | void => {
             const { loadable } = props;
-            if (!loadable) {
-                return;
-            }
             let promise: IRequestResult | void;
             if (beforeSend) {
                 promise = beforeSend(props);
+            }
+            if (!loadable) {
+                return promise;
             }
             const { options } = props;
             const loadingComponent = getLoading(options);
             if (loadingComponent) {
                 loadingComponent.start();
             }
+            return promise;
         };
 
         //关闭加载效果
@@ -49,7 +50,10 @@ function loadingExtend({
             props: { success: boolean }
         ): void => {
             responseEnd(xhr, _opts, props);
-            const { options } = _opts;
+            const { options, loading } = _opts;
+            if (!loading) {
+                return;
+            }
             const loadingComponent = getLoading(options);
             if (loadingComponent) {
                 loadingComponent.finish();
